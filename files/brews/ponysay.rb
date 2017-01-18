def610bc21e8ddd5d6d1bc7c0a5b33045d80bc12
Andre Arko <andre@arko.net>
2017-01-18 12:50:04 -0800

    update to sha256 for homebrew

--- files/brews/ponysay.rb
+++ files/brews/ponysay.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ponysay < Formula
   homepage 'https://github.com/erkin/ponysay'
   url 'http://files.arko.net/1n0o0g2T122I/ponysay-master.zip'
-  sha1 '37ba83189bb3a577bff7f0ad435b78f58e8ebc25'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
   version 'e7c03f'
 
   depends_on 'python3'
